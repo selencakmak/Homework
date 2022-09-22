20610657bc773f78c672c42527803b408ab3dae4
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-22 14:55:55 +0300

    Java Camp

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,23 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=12;
+		sayi=-129;
+		//primitive types
+		byte sayi1 =127;
+		//byte -128 ve 127 arasý 
+		double sayi3 = 12.5;
+		char karakter = 'A';
+		String sehir= "Ankara";
+		boolean dogruMu = false;
+		
+		
+		
+		
+		
+
+	}
+
+}
