0023f2063057e0e1f031cb98d00cd8ca60dd364b
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-22 14:37:59 +0300

    Second

---
+++ variables1/src/variables1/Main.java
@@ -0,0 +1,18 @@
+package variables1;
+//reusability
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// case sensitive (buyuk kucuk harf duyarli)
+		// degiskenlerde camelCase
+		int ogrenciSayisi = 10 ;
+		String mesaj = "Öðrenci sayýsý : ";
+		
+		System.out.println(mesaj  + ogrenciSayisi);
+		System.out.println(mesaj  + ogrenciSayisi);
+		System.out.println(mesaj  + ogrenciSayisi);
+		
+	}
+
+}
