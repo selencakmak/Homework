5745bc6e69ae326ddf6feeb640649cf73953e3af
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-22 22:28:24 +0300

    Java Camp

---
+++ arraysDemo/src/arraysDemo/Main.java
@@ -0,0 +1,35 @@
+package arraysDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//diziler ayný tipteki 
+		String ogrenci1="Engin";
+		String ogrenci2="Derin";
+		String ogrenci3="Salih";
+		
+		System.out.println(ogrenci1);
+		System.out.println(ogrenci2);
+		System.out.println(ogrenci3);
+		
+		System.out.println("---------------------------");
+		
+		String[] ogrenciler = new String[4];
+		ogrenciler[0]="Engin";
+		ogrenciler[1]="Derin";
+		ogrenciler[2]="Salih";
+		ogrenciler[3]="Ahmet";
+		
+		
+		for(int i=0;i<ogrenciler.length;i++) {
+			System.out.println(ogrenciler[i]);
+		}
+		System.out.println("--------------------------");
+		//Farklý bir kullaným
+		
+		for(String ogrenci : ogrenciler) {
+			System.out.println(ogrenci);
+		}
+	}
+
+}
