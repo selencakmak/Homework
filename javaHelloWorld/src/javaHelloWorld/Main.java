f11805b0f92f4210764d5a036b99e38a49846200
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-22 14:08:59 +0300

    First

---
+++ javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,13 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// yorum satýrý
+		System.out.println("Hello Java");
+		System.out.println("Hello Java2");
+		
+		
+	}
+
+}
