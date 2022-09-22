ec67619ee564d126c638554b7a4c74cee19f30cf
selencakmak <76444550+selencakmak@users.noreply.github.com>
2022-09-22 15:08:07 +0300

    Java Camp

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,21 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi =20;
+		
+		if(sayi <20) {
+		System.out.println("Sayi 20den küçüktür.");
+		}
+		
+		else if (sayi==20){
+			System.out.println("Sayi 20ye eþittir.");	
+		}
+		
+		else {
+			System.out.println("Sayi 20den büyüktür.");
+		}
+	}
+
+}
